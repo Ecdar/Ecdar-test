61446caa6f38dfb11551dcab4a45e849396d428f
Thorulf <thorulf@live.dk>
2021-08-02 14:26:14 +0200

    Solved comments

--- src/main/kotlin/proofs/ConsistentCompositions.kt
+++ src/main/kotlin/proofs/ConsistentCompositions.kt
@@ -7,22 +7,19 @@ import java.util.*
 
 class ConsistentCompositions : Proof {
     override fun search(component: System, ctx: ProofSearcher.IterationContext) {
-        if (component.isLocallyConsistent.isPresent && component is Component){
+        if (component.isLocallyConsistent.orElse(false) && component is Component){
 
             for (other in ctx.dirtyComponents) {
-                if (other.isLocallyConsistent.isPresent && other is Component){
+                if (other.isLocallyConsistent.orElse(false) && other is Component){
                     if (component.prefix != other.prefix){
                         //Only allow compositions from components from the same project
-                        continue;
+                        continue
                     }
 
                     val comp = ctx.addNewComponent(Composition(component, other))
 
                     if (comp.isLocallyConsistent.isEmpty){
-                        val isConsistent = component.isLocallyConsistent.get() && other.isLocallyConsistent.get()
-                        comp.isLocallyConsistent = Optional.of(isConsistent)
-
-                        println("$component || $other  =  $comp")
+                        comp.isLocallyConsistent = Optional.of(true)
 
                         ctx.setDirty(component)
                         ctx.setDirty(other)
