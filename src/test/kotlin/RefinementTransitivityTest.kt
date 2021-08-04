f7f3b58720ac9cca15c7b0139718f632323591ed
Sebastian Lund <sebastianlund112@gmail.com>
2021-08-04 06:14:11 +0200

    Add debug print of number of relations

--- src/test/kotlin/RefinementTransitivityTest.kt
+++ src/test/kotlin/RefinementTransitivityTest.kt
@@ -11,11 +11,13 @@ internal class RefinementTransitivityTest {
     @Test
     fun visit1() {
         println("Refinements: " + refinementCount(RelationLoader.relations))
+        println("Components: ${RelationLoader.relations.size}")
 
         var searcher = ProofSearcher()
         var newRelation = searcher.findNewRelations(RelationLoader.relations)
 
         println("Refinements: " + refinementCount(newRelation))
+        println("Components: ${newRelation.size}")
     }
 
     fun refinementCount(components: ArrayList<System>): Int{
